d4b33dcde343b292dc0fcc49111e8b6abb1cf4ff
Daianes26 <daianeasilva26@gmail.com>
2019-06-29 18:32:25 -0400

    Add favorites - need work

--- shelfapp/models.py
+++ shelfapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 # Create your models here.
 
 class Category(models.Model):
--- shelfapp/views.py
+++ shelfapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Book, Category
+from .models import Book, Category, Favorite
 from django.views import generic
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -43,15 +44,16 @@ def user_favorites(request):
        'favorites_list': favorites_list,
    }
 
-   return render(request, 'shelfapp/added_favorites.html', context=context)
+   return render(request, 'shelfapp/added_favorites.html', context)
 
 
 @login_required
 def add_to_favorites(request, pk):
-   book = get_object_or_404(Book, pk=pk)
+    book = get_object_or_404(Book, pk=pk)
 
-   new_favorite, created = Favorite.objects.get_or_create(book=book, user=request.user)
-   if not created:
+    new_favorite, created = Favorite.objects.get_or_create(book=book, user=request.user)
+    
+    if not created:
         new_favorite.delete()
 
         context = {
@@ -60,28 +62,4 @@ def add_to_favorites(request, pk):
         'created': created,
         }
 
-   return render(request, 'shelfapp/favorite_added.html', context=context)
-
-# def category(request):
-
-#     category_list = Category.objects.all()
-
-#     context = {
-#         'category_list': category_list,
-#     }
-
-#     return render(request, 'shelfapp/categories.html', context=context)
-
-
-# def list_of_books_by_category(request, category_pk):
-  
-#     books_by_category = Book.objects.filter(categories__id=category_pk)
-
-#     context = {
-#         'books_by_category': books_by_category,
-
-#     }
-
-#     return render(request, 'shelfapp/categories_list.html', context=context)
-
-
+    return render(request, 'shelfapp/favorite_added.html', context)
